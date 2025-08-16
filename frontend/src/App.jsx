836d10b3519a154e3824a382194fd428af5cd217
Roman Izegov <izegovrg@yandex.ru>
2025-08-16 11:14:07 +0300

    implemented protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,13 @@ import Register from './components/Register'
 import Login from './components/Login'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import MyJobs from './components/MyJobs'
+import ProtectedRoute from './components/ProtectedRoute'
+import RoleRoute from './components/RoleRoute'
+import MyOffers from './components/MyOffers'
+import Jobs from './components/Jobs'
+import JobDetails from './components/JobDetails'
+import JobOffer from './components/JobOffer'
 
 function App() {
  
@@ -15,6 +22,40 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route
+          path="/my-jobs"
+          element={
+            <ProtectedRoute>
+              <MyJobs/>
+            </ProtectedRoute>
+          }
+          />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
+          <Route
+            path="/my-jobs"
+            element={
+              <RoleRoute allowed={['client']}>
+                <MyJobs />
+              </RoleRoute>
+            }
+          />
+          <Route
+            path="/jobs/:id/offers"
+            element={
+              <RoleRoute allowed={['client']}>
+                <JobOffer />
+              </RoleRoute>
+            }
+          />
+          <Route
+            path="/my-offers"
+            element={
+              <RoleRoute allowed={['worker']}>
+                <MyOffers />
+              </RoleRoute>
+            }
+          />
         </Routes>
       </div>
     </>
